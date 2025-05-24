9e114da8980b3137d64330a3a9004b77c0a1f9f0
charole <rjs985939@gmail.com>
2025-05-24 16:44:43 +0900

    CHORE: 애플 로그인/다른 로그인 수단 제거

--- lib/pages/auth/auth_page.dart
+++ lib/pages/auth/auth_page.dart
@@ -106,44 +106,6 @@ class AuthPage extends StatelessWidget {
               ),
 
               const SizedBox(height: 16),
-              // 애플로 가입
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/register');
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color(0xFF000000),
-                    padding: const EdgeInsets.symmetric(vertical: 16),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                  ),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.asset(
-                        'assets/images/apple_logo.png',
-                        height: 24,
-                        width: 24,
-                      ),
-                      const SizedBox(width: 8),
-                      const Text(
-                        'Apple로 가입',
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.white,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-
-              const SizedBox(height: 16),
               // 이메일로 가입
               SizedBox(
                 width: double.infinity,
@@ -179,55 +141,6 @@ class AuthPage extends StatelessWidget {
                     ],
                   ),
                 ),
-              ),
-
-              const SizedBox(height: 30),
-              const Row(
-                children: [
-                  Expanded(child: Divider(color: Colors.grey, thickness: 1)),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 10),
-                    child: Text(
-                      '다른 방법으로 가입하기',
-                      style: TextStyle(
-                        color: Colors.grey,
-                        fontSize: 14,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                  ),
-                  Expanded(child: Divider(color: Colors.grey, thickness: 1)),
-                ],
-              ),
-
-              const SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.pushNamed(context, '/register');
-                      // TODO: Google 로그인 로직
-                    },
-                    child: Image.asset(
-                      'assets/images/google_logo.png',
-                      width: 40,
-                      height: 40,
-                    ),
-                  ),
-                  const SizedBox(width: 24),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.pushNamed(context, '/register');
-                      // TODO: Facebook 로그인 로직
-                    },
-                    child: Image.asset(
-                      'assets/images/facebook_logo.png',
-                      width: 40,
-                      height: 40,
-                    ),
-                  ),
-                ],
               ),
 
               const SizedBox(height: 20),
